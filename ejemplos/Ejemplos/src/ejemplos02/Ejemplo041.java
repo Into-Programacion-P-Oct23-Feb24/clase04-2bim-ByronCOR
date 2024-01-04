989c96957d1c2fa2744367bec19e3da41eb8bb0a
unknown <bdcordova@utpl.edu.ec>
2024-01-04 15:08:50 -0500

    cambios

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -14,7 +14,7 @@ public class Ejemplo041 {
     public static void main(String[] args) {
         int[][] primerValor = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
         int[][] segundoValor = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
-        
+
         obtenerMultiplicacion(primerValor, segundoValor);
 
     }
@@ -23,14 +23,14 @@ public class Ejemplo041 {
         int[][] arreglo3 = new int[3][3];
         int resultado;
         for (int i = 0; i < a.length; i++) {
+            String cadena = "";
             for (int j = 0; j < a[i].length; j++) {
-                resultado = a[i][j]*b[i][j];
-                arreglo3[i][j]= resultado;             
+                resultado = a[i][j] * b[i][j];
+                arreglo3[i][j] = resultado;
+                cadena = String.format("%s %d", cadena, arreglo3[i][j]);
             }
-            System.out.printf("%d\n",arreglo3[i][j]);
+            System.out.printf("%s\n", cadena);
         }
-      
-   
 
     }
 
